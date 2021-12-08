1d7da9426be6a8b38d7ea4c833dfb4a5725cbac2
Keydouu <63008369+Keydouu@users.noreply.github.com>
2021-12-08 18:48:59 +0100

    Add files via upload

---
+++ Day4_p2.java
@@ -0,0 +1,99 @@
+package AdventOfCode2021;
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+
+import java.util.List;
+
+public class Day4{
+	public static void main(String[] args) {
+		File fp = new File("/home/keydou/AdventOfCode2021/Day4_input.txt");
+		List<BingoTable> bT = new ArrayList<BingoTable>();
+		List<Integer> drawnNumbers = new ArrayList<Integer>();
+		char c='a';
+		int newNumber,i=0,n=0,numberOfTables=0,winnerScore=0;
+		try {
+			Scanner fileScanner = new Scanner(fp);
+			String firstLine;
+			firstLine=fileScanner.nextLine();
+			String[] strarray = firstLine.split(",");
+			for(String elm: strarray)
+				drawnNumbers.add(Integer.parseInt(elm));
+			while(fileScanner.hasNextInt()){//read bingo tables
+				bT.add(new BingoTable());
+				for (int j=0;j<25;j++) {
+					newNumber=fileScanner.nextInt();
+					bT.get(numberOfTables).addToTable(newNumber,j);
+				}
+				numberOfTables++;
+			}
+			fileScanner.close();
+		}
+		catch(Exception e) {
+			e.getStackTrace();
+			System.out.print(e.getMessage());
+		}
+		boolean [] loosingTables = new boolean[numberOfTables];
+		for (i=0;i<numberOfTables;i++){
+			loosingTables[i]=true;
+		}
+		for (newNumber=0;(newNumber<drawnNumbers.size());newNumber++){
+			for (i=0;(i<numberOfTables);i++){
+				if(loosingTables[i]){
+					winnerScore=bT.get(i).addRevelation(drawnNumbers.get(newNumber));
+					if((winnerScore>0)&&(loosingTables[i])){
+						n=winnerScore;
+						loosingTables[i]=false;
+					}
+				}
+			}
+		}
+		System.out.println("score of last winning board "+n);
+	}
+}
+class BingoTable{
+	private int[][] numbersTable = new int[5][5];
+	private boolean[][] boolTable = new boolean[5][5];
+	public void addToTable(int value,int position){
+		this.numbersTable[position/5][position%5]=value;
+		this.boolTable[position/5][position%5]=false;
+	}
+	public int addRevelation(int newNumber){
+		int score =0;
+		for(int i=0;i<5;i++){
+			for(int j=0;j<5;j++){
+				if(this.numbersTable[i][j]==newNumber){
+					boolTable[i][j]=true;
+					if (this.win(i,j)){
+						for(i=0;i<5;i++){
+							for (j=0;j<5;j++){
+								if(!this.boolTable[i][j])
+									score+=this.numbersTable[i][j];
+							}
+						}
+						score*=newNumber;
+					}
+					i=5;j=5;
+				}
+			}
+		}
+		return score;
+	}
+	public boolean win(int y, int x){
+		boolean verticalWin=true,horizontalWin=true;
+		for(int i=0;(i<5)&&(verticalWin||horizontalWin);i++){
+
+			if(!this.boolTable[y][i])
+				horizontalWin=false;
+
+			if(!this.boolTable[i][x])
+				verticalWin=false;
+
+		}
+		if((verticalWin)||(horizontalWin)){
+			return true;
+		}
+		else
+			return false;
+	}
+}
